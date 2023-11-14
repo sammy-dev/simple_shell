cfe7aaea2ebdd7121568a3e72d86ca9bddb04557
Sammy Mulandi <mulandisammy419@gmail.com>
2023-11-14 00:56:40 -0800

    main.c

---
+++ main.c
@@ -0,0 +1,45 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	info_t info[] = { INFO_INIT };
+	int q = 2;
+
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=i" (q)
+		: "i" (q));
+
+	if (ac == 2)
+	{
+		q = open(av[1], O_RDONLY);
+		if (q == -1)
+		{
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = q;
+	}
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
+}
+
