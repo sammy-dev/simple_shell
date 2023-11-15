8749614906ef8d9b74bcdc742839c4e03d1f97aa
Sammy Mulandi <104715306+sammy-dev@users.noreply.github.com>
2023-11-15 19:07:52 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -183,9 +183,7 @@ int _myhistory(info_t *);
 int _myalias(info_t *);
 
 /*toem_getline.c */
-ssize_t buffer_input(info_t *info, char **commandBuffer, size_t *bufferSize);
-int _getline(info_t *, char **, size_t *);
-void sigintHandler(int);
+int custom_getline(info_t *info, char **ptr, size_t *length);
 ssize_t get_user_input(info_t *info);
 void sigintHandler(__attribute__((unused))int sig_num);
 
