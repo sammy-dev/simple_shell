8618375b49a452d63f754dc96540bbb484cc10b9
Sammy Mulandi <mulandisammy419@gmail.com>
2023-11-15 03:14:59 -0800

    getenv.c file

---
+++ getenv.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+ * get_environment - returns the string array copy of our environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+char **get_environment(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _remove_env_var - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *                  constant function prototype.
+ * Return: 1 on delete, 0 otherwise
+ * @var: the string env var property
+ */
+int _remove_env_var(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t index = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), index);
+			index = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		index++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _set_env_var - Initialize a new environment variable,
+ *                or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * @var: the string env var property
+ * @value: the string env var value
+ * Return: Always 0
+ */
+int _set_env_var(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
